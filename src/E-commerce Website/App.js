d6da6ec6c64fd5675f061198a50ed1da1b9773b8
Soylatte <kksoyeon61@gmail.com>
2023-10-20 22:51:04 +0900

    User event

--- src/E-commerce Website/App.js
+++ src/E-commerce Website/App.js
@@ -5,3 +5,26 @@ function () {
         </div>
     )
 }
+
+function(ModeToggler) {
+    let darkModeOn = false;
+    const darkMode = <h1>DarkMode is On</h1>
+    const lightMode = <h1>LightMode is On</h1>
+    function handleClick() {
+        darkModeOn = !true;
+        if (darkModeOn === true) {
+            console.log("DarkMode is On")
+        } else {
+            console.log("LightMode is On")
+        }
+    }
+
+    return (
+        <div>
+            {darkModeOn ? darkMode : lightMode}
+            <button onClick={handleClick}>Click Me</button>
+        </div>
+    )
+
+}
+export default ModeToggler;

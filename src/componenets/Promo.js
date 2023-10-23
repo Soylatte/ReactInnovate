6d5261dcab136a4bc41d7ccab684cf058605dfaf
Soylatte <kksoyeon61@gmail.com>
2023-10-23 21:48:47 +0900

    promo function

--- src/componenets/Promo.js
+++ src/componenets/Promo.js
@@ -11,4 +11,17 @@ function Promo() {
         </div>
     )
 }
+
+const data = {
+    heading: "99% off all items!!"
+    callToAction: "Everything must go"
+}
+function Promo() {
+    return (
+        <div>
+            <PromoHeading heading={data.heading}
+                callToAction={data.callToAction} />
+        </div>
+    )
+}
 export default Promo;
